6619991de26763aaf4e54dc30d5809dfc256599e
Utkatsh Kaule <utkarshkaule7@gmail.com>
2024-03-14 19:58:07 +0530

    done

---
+++ controllers/Course.js
@@ -0,0 +1,3 @@
+const Course = require('../models/Course')
+const Tag = require("../models/Tags")
+const Uesr = require("../models/User")
--- models/Course.js
+++ models/Course.js
@@ -14,7 +14,7 @@ const courseSchema = new mongoose.Schema({
         type:String,
         required:true,
         trim:true
-    },
+    }, 
     whatYouWillLearn:{
         type:String,
         trim:true,
